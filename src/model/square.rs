2dbe2d1f565d82e86a1ee58ec597769989fd99ab
Nitepone <admin@night.horse>
2018-05-26 17:50:01 -0400

    Finished basic Square object

--- src/model/square.rs
+++ src/model/square.rs
@@ -4,18 +4,59 @@
 // Distributed under terms of the MIT license.
 //
 
-final int MAX_ADJACENT = 8;
+const MAX_ADJACENT: u8 = 8;
 
 pub enum Status{
-    Openned,
+    Opened,
     Flagged,
     Covered
+}
+
+pub enum Contents{
+    Empty,
+    Mine
 }
 
 pub struct Square{
     adjacent_squares: Square[MAX_ADJACENT],
     status: Status,
-    value: int,
+    contents: Contents,
+    cached_adjacent_mines: u8
+}
+
+impl Square{
+    // Adds an adjacent Square to a Square
+    pub fn add_adjacent(Square: adjacent){
+        for i in self.adjacent_squares.iter() {
+            if self.adjacent_squares[i] == null {
+                self.adjacent_squares[i] = adjacent;
+            }
+        }
+    }
+
+    // Opens a square if it is not already
+    // Returns false if the square is Opened, otherwise true
+    pub fn open(){
+        if self.status == Status:Opened {
+            return false;
+        }
+        self.status = Status::Opened;
+        return true;
+    }
+
+    // Toggles the flag status
+    // Square must be in the Covered or Flagged state
+    // Returns false if the flag is not Covered or Flagged, otherwise true
+    pub fn toggle_flag(){
+        match self.Status {
+            Status::Covered => self.status = Status::Flagged,
+            Status::Flagged => self.status = Status::Covered,
+            //Catch other Status (Can't flag Opened tiles)
+            _ => return false,
+        }
+        //If we get here, we had success
+        return true;
+    }
 }
 
 
